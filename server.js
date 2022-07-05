f65eba944f4c6f61b3028bcbe075377fb60f17b3
dev-hail <hardik.dev.08@gmail.com>
2022-07-05 15:51:35 +0530

    Changing port

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 
+app.get('/', (req, res) => {res.send("it is working!")})
 app.post('/signin', (req,res) => {signin.handleSignin(req, res, bcrypt, db)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, bcrypt, db) })
 app.get('/profile/:id', (req, res, db) => {profile.handleProfile(req, res, db)})
